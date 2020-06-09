686528618fe11e79f66e7ac2c0a51946b5faf1e5
Zone Cảnh <canhzone861998@gmail.com>
2020-06-09 11:00:34 +0700

    Thêm giao diện lỗi 500, bắt lỗi trong function index trong controller products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,41 +2,50 @@
 var Book = require('../models/book.model');
 
 //var oldPage = 0;
-module.exports.index = async (req, res) => {
-//   var currentPage = parseInt(req.query.page) || 1; //n
-//   var perPage = 8; //x
-  
-//   var start = (currentPage - 1) * perPage;
-//   var end = currentPage * perPage;
-//   var products = db.get('books').value();
-//   var totalPage = Math.ceil(products.length / perPage);
-  
-//   if (currentPage > totalPage) {
-//     currentPage = totalPage
-//   } else if (currentPage < 1) {
-//     currentPage = 1
-//   }
-  
-  //cách 1: dùng arr.slice để cắt arr
-  // var products = db.get('books').value();
-  // res.render("products/index", {products: products.slice(start, end), 
-  //                              currentPage: currentPage, 
-  //                              totalPage: totalPage,
-  //                              //oldPage: oldPage
-  //                              });
-  
-  //oldPage = currentPage;
-  
-  //cách 2: dùng hàm take và drop của thư viện Lodash
-  // var drop = (currentPage - 1) * perPage;
-  // var products = db.get('books').drop(drop).take(perPage).value();
-  // res.render("products/index", {products: products, 
-  //                               currentPage: currentPage, 
-  //                               totalPage: totalPage});
-  var products = await Book.find();
-  res.render("products/index", {
-    products: products
-  });
+module.exports.index = async (req, res, next) => {
+  try {
+    //   var currentPage = parseInt(req.query.page) || 1; //n
+    //   var perPage = 8; //x
+
+    //   var start = (currentPage - 1) * perPage;
+    //   var end = currentPage * perPage;
+    //   var products = db.get('books').value();
+    //   var totalPage = Math.ceil(products.length / perPage);
+
+    //   if (currentPage > totalPage) {
+    //     currentPage = totalPage
+    //   } else if (currentPage < 1) {
+    //     currentPage = 1
+    //   }
+
+    //cách 1: dùng arr.slice để cắt arr
+    // var products = db.get('books').value();
+    // res.render("products/index", {products: products.slice(start, end), 
+    //                              currentPage: currentPage, 
+    //                              totalPage: totalPage,
+    //                              //oldPage: oldPage
+    //                              });
+
+    //oldPage = currentPage;
+
+    //cách 2: dùng hàm take và drop của thư viện Lodash
+    // var drop = (currentPage - 1) * perPage;
+    // var products = db.get('books').drop(drop).take(perPage).value();
+    // res.render("products/index", {products: products, 
+    //                               currentPage: currentPage, 
+    //                               totalPage: totalPage});
+    var products = await Book.find();
+    var a;
+    a.b();
+    res.render("products/index", {
+      products: products
+    });
+  } catch (error) {
+    next(error);
+    res.render('error/500', {
+      error: error
+    })
+  }
 };
 
 module.exports.search = async function(req, res) {
