5ca04ef14aacf28eb1a7aa1b7735e225c0b1ca92
Zone Cảnh <canhzone861998@gmail.com>
2020-06-09 11:47:16 +0700

    Thêm biến process.env.PORT

--- server.js
+++ server.js
@@ -83,6 +83,6 @@ app.use("/auth", authRoute);
 app.use("/cart", cartRoute);
 
 // listen for requests :)
-app.listen(3000, () => {
-  console.log("Server listening on port " + 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server listening on port " + process.env.PORT || 3000);
 });
